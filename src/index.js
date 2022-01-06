80db7a8beebde9855f963b6889883c58320d8e3f
Maria Guchkova <mariaguchkova1@gmail.com>
2022-01-06 08:34:02 +0300

    feat: implement missed functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let arrNum = Math.abs(n).toString().split("");
+    let newArr = [];
+    for (let i = 0; i < arrNum.length; i++) {
+        newArr.unshift(arrNum[i]);
+    }
+    let str = newArr.join("");
+    let result = Number(str);
+    return result;
+};
